4aaef55ad335cb742f0af27faa47e1abcd4d1087
Quinton Baudoin <baudoin.q@gmail.com>
2015-09-03 18:11:10 -0500

    more commitssssssssssx

---
+++ Prototype/Assets/Scripts/NetworkTurn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkTurn : playerNetworkSetup
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
