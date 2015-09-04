e34a888959388fdd1a66ca88fd0e1d41656aa31c
Quinton Baudoin <baudoin.q@gmail.com>
2015-09-04 16:09:43 -0500

    added network stuff

---
+++ Prototype/Assets/Scripts/NetworkConnectionHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkConnectionHandler : MonoBehaviour {
+	public virtual void OnServerDisconnect(NetworkConnection conn)
+	{
+		NetworkServer.DestroyPlayersForConnection(conn);
+	}
+
+ 
+}
+
+
